6c7bc268062f295c173dcab2d8c1d8472ee94b8a
The DT <duongtuan30306@gmail.com>
2026-03-12 18:05:05 +0700

    Refactor getCustomConfig to remove hardcoded values
    Removed several hardcoded URLs and keys from the custom configuration function.

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -31,26 +31,11 @@ export interface NuxtConfig {
 
 export function getCustomConfig(url: URL, originalConfig?: NuxtConfig | null): NuxtConfig {
     const config: NuxtConfig = originalConfig ? structuredClone(originalConfig) : {};
-    
-    if (!config.public) config.public = {};
-    config.public.environmentType = "stable";
     config.public.apiUrl = url.origin + "/api";
     config.public.baseUrl = url.origin;
     config.public.cdnEdge = url.origin;
     config.public.cdnOrigin = url.origin + "/uploads";
-    config.public.embedUrl = "https://e-embed.mangadex.org";
-    config.public.kcUrl = url.origin + "/keycloak";
-    config.public.kcClientId = "mangadex-frontend-stable";
-    config.public.forumsUrl = "https://forums.mangadex.org";
-    config.public.paymentsUrl = "https://payment.mangadex.org";
-    config.public.gtmId = "";
-    config.public.recaptchaSiteKey = "6LflOrIaAAAAACcpRSiKQlt_X6bq-QcVjHTG1diJ";
-    config.public.stripeManageUrl = "https://billing.stripe.com/p/login/14A4gz6uFeba8Z77u68so00";
-    
-    if (!config.app) config.app = {};
-    config.app.baseURL = "/";
-    config.app.buildAssetsDir = "/_nuxt/";
-    config.app.cdnURL = "";
+    config.public.kcUrl = url.origin + "/keycloak";    
     
     return config;
 }
