7dcdaffbd6ab44fc144d2e8fa9343f39d79ead60
thedtvn <duongtuan30306@gmail.com>
2026-06-16 05:45:17 +0700

    Lol baseline since March 2023 Mangadex: Any browser (or OS) updated in the last ~10 years should be fine.

--- src/constants/headers.ts
+++ src/constants/headers.ts
@@ -1,7 +1,9 @@
 export const DEFAULT_HEADERS = {
+    "accept": "application/json, text/plain, */*",
+    "priority": "u=0, i",
     "User-Agent": "Mozilla/5.0 (compatible; MangadexEgdeProxy/1.0; +https://github.com/thedtvn/mangadex-edge-proxy)",
-    Accept: "application/json, text/plain, */*",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Cache-Control": "no-cache",
-    Pragma: "no-cache"
+    "sec-fetch-dest": "empty",
+    // "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "upgrade-insecure-requests": "1"
 };
