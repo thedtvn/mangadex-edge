8d41c5ba1a6fe1a356cef4619dc57323481a1add
thedtvn <duongtuan30306@gmail.com>
2026-06-16 05:46:18 +0700

    Corect project url

--- src/constants/headers.ts
+++ src/constants/headers.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_HEADERS = {
     "accept": "application/json, text/plain, */*",
     "priority": "u=0, i",
-    "User-Agent": "Mozilla/5.0 (compatible; MangadexEgdeProxy/1.0; +https://github.com/thedtvn/mangadex-edge-proxy)",
+    "User-Agent": "Mozilla/5.0 (compatible; MangadexEgdeProxy/1.0; +https://github.com/thedtvn/mangadex-edge)",
     "sec-fetch-dest": "empty",
     "sec-fetch-mode": "navigate",
     "sec-fetch-site": "same-origin",
